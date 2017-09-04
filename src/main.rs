5a3d14aed6b852ecf50571761b7890b3d1d93713
B <ben01189998819991197253@users.noreply.github.com>
2017-09-04 08:40:52 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+extern crate clap;
+extern crate rand;
+
+mod notes;
+
+use clap::{App, Arg};
+
+use notes::*;
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const APP_NAME: &'static str = "fretta";
+
+fn main() {
+    let matches = App::new(APP_NAME)
+        .version(VERSION)
+        .about("Tool for training one's knowledge of the fretboard")
+        .arg(Arg::with_name("tuning")
+            .short("t")
+            .long("tuning")
+            .value_name("TUNING")
+            .help("Comma-separated list of notes representing the tuning of the instrument")
+            .takes_value(true))
+        .get_matches();
+}
---
+++ src/notes/mod.rs
@@ -0,0 +1,14 @@
+pub enum Notes {
+    A,
+    ASharp,
+    B,
+    C,
+    CSharp,
+    D,
+    DSharp,
+    E,
+    F,
+    FSharp,
+    G,
+    GSharp,
+}
