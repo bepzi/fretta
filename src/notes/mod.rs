4a320ffbbd42bdea142081dbe6aa080ed83de2af
B <ben01189998819991197253@users.noreply.github.com>
2017-09-04 09:00:58 -0500

    Implement try_from_string for Note

--- src/notes/mod.rs
+++ src/notes/mod.rs
@@ -1,4 +1,6 @@
-pub enum Notes {
+use errors::*;
+
+pub enum Note {
     A,
     ASharp,
     B,
@@ -13,8 +15,24 @@ pub enum Notes {
     GSharp,
 }
 
-impl Notes {
-//    pub fn try_from_string<S: Into<String>>(s: S) -> Result<Notes, Error> {
-//        Ok(Notes::A)
-//    }
+impl Note {
+    pub fn try_from_string<S: Into<String>>(s: S) -> Result<Note> {
+        let s = s.into();
+
+        match s.as_ref() {
+            "A" => Ok(Note::A),
+            "A#" | "Bb" => Ok(Note::ASharp),
+            "B" => Ok(Note::B),
+            "C" => Ok(Note::C),
+            "C#" | "Db" => Ok(Note::CSharp),
+            "D" => Ok(Note::D),
+            "D#" | "Eb" => Ok(Note::DSharp),
+            "E" => Ok(Note::E),
+            "F" => Ok(Note::F),
+            "F#" | "Gb" => Ok(Note::FSharp),
+            "G" => Ok(Note::G),
+            "G#" | "Ab" => Ok(Note::GSharp),
+            _ => bail!("Unrecognized note: {}", s),
+        }
+    }
 }
