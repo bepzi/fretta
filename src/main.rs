47a567e88fe5c9eb5d00aeb382617288978ca638
B <ben01189998819991197253@users.noreply.github.com>
2017-09-04 08:49:18 -0500

    Implement error-chain

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,43 @@
+// `error_chain!` can recurse deeply
+#![recursion_limit = "1024"]
+
 extern crate clap;
+#[macro_use]
 extern crate error_chain;
 extern crate rand;
 
 mod notes;
+use notes::*;
+
+mod errors {
+    error_chain! { }
+}
+use errors::*;
 
 use clap::{App, Arg};
-
-use notes::*;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 const APP_NAME: &'static str = "fretta";
 
 fn main() {
+    if let Err(ref e) = run() {
+        println!("error: {}", e);
+
+        for e in e.iter().skip(1) {
+            println!("caused by: {}", e);
+        }
+
+        // The backtrace is not always generated. Try to run this example
+        // with `RUST_BACKTRACE=1`.
+        if let Some(backtrace) = e.backtrace() {
+            println!("backtrace: {:?}", backtrace);
+        }
+
+        ::std::process::exit(1);
+    }
+}
+
+fn run() -> Result<()> {
     let matches = App::new(APP_NAME)
         .version(VERSION)
         .about("Tool for training one's knowledge of the fretboard")
@@ -22,4 +48,6 @@ fn main() {
             .help("Comma-separated list of notes representing the tuning of the instrument")
             .takes_value(true))
         .get_matches();
+
+    Ok(())
 }
